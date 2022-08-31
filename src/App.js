d4efbfc3dc6a9e0db87aeb63509ead131670a25e
lmoraesdev <lbc_moraes@outlook.com>
2022-08-30 23:24:36 -0300

    refactor: stylizado com style components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobolStyle } from "./Components/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobolStyle />
       <Cabecalho />
       <Container />
     </>
