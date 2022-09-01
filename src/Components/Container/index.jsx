0fc7db9b134f067a0d85db1f102f7ea8daa7a439
lmoraesdev <lbc_moraes@outlook.com>
2022-08-31 23:10:58 -0300

    refactor: css para style components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
+const ContainerWrapper = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+  @media (max-width: 800px){
+    flex-direction: column;
+  }
+`;
+
 export default function Container(){
   return (
-    <div className="container">
+    <ContainerWrapper>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </ContainerWrapper>
   );
 };
