b632f64f1342841c759a1cfa74e31033f945b2fe
lmoraesdev <lbc_moraes@outlook.com>
2022-09-03 17:35:41 -0300

    feat: Icone claro e escuro

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import SwitcherTema from "./Components/switcherTema";
+import { useState } from "react";
+
 
 function App() {
+  const [tema,setTema] = useState(true)
+  const toggleTema = () => {
+    setTema((tema) = !tema);
+  }
+
   return (
     <>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
+    </ThemeProvider>
     </>
   );
 }
